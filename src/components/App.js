14dbacc04cc9e6f124b372a19e3eb7e440082e17
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-03-22 07:18:47 +0300

    finished main page

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Home from './Home';
+import Main from './Main';
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path='/' element= {<Home />} />
+        <Route path='/main' element = {<Main />} />
       </Routes>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,33 @@
-
+import { Link } from "react-router-dom"
 
 export default function Main() {
     return (
         <div className="main">
-            this is main
+            <div className="main-top">
+                <header className="main-header">
+                    <h2>Uradi Encore Resort</h2>
+                    <div className="main-header-tabs">
+                        <Link to="/about" >About Us</Link>
+                        <Link to="/" >Home</Link>
+                    </div>
+                </header>
+                <div className="main-top-body">
+                    <p>Create an account with us and enjoy all the luxury we have to offer</p>
+                    <span>
+                        <Link to="/signup" >Sign up</Link>
+                        <Link to="/login" >Log in</Link>
+                    </span>
+                </div>
+            </div>
+            <div className="main-bottom">
+                <h4>Our deals</h4>
+                <div className="main-bottom-body">
+                    <Link to="/main/foods" ><img loading="lazy" src="./images/foods.jpg" alt="delicacies" /> <p>Delicacies</p></Link>
+                    <Link to="/main/rooms" ><img loading="lazy"  src="./images/rooms.jpg" alt="hotel rooms" /> <p>Rooms</p></Link>
+                    <Link to="/main/games" ><img loading="lazy"  src="./images/games.jpg" alt="games" /> <p>Games</p></Link>
+                    <Link to="/main/scenary" ><img loading="lazy"  src="./images/scenary.jpg" alt="scenes" /> <p>Scenary</p></Link>
+                </div>
+            </div>
         </div>
     )
 }
