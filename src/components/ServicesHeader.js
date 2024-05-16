b63933c720c1cf12cab984dd7bed019e82d57460
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-05-16 16:24:50 +0300

    added logout button

--- src/components/ServicesHeader.js
+++ src/components/ServicesHeader.js
@@ -1,5 +1,17 @@
 import { Link } from "react-router-dom"
+import { AuthContext } from "../helpers/AuthContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 export default function Services({ checkFood }) {
+    const {setLoginState, setAuthState} = useContext(AuthContext);
+    const history = useNavigate();
+    function logout() {
+        localStorage.removeItem("accessToken")
+        setAuthState(false);
+        setLoginState("Logout Succesful");
+        history('/');
+
+    }
     return (
         <header className="services-header">
             <Link to="/"><img src='../images/uradi-logo2.png' alt='hotel-logo' /></Link>
@@ -12,6 +24,7 @@ export default function Services({ checkFood }) {
                 <Link to="/main/rooms">Rooms</Link>
                 <Link to="/main/games">Activities</Link>
                 <Link to="/about">About Us</Link>
+                <button className="logout-btn" style={{display: "none"}} onClick={logout}>Log out</button>
             </div>
         </header>
     )
