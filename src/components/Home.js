91d3d0aafc9dab859cb7d4eaa5037d768dca41f0
Jimmy21048 <118855884+Jimmy21048@users.noreply.github.com>
2024-05-18 12:39:09 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ export default function Home() {
 
     useEffect(() => {
         // axios.get("http://localhost:3001/login/auth", {
-        axios.get("https://uradi-encore-server.onrender.com/auth", {
+        axios.get("https://uradi-encore-server.onrender.com/login/auth", {
           headers : {
             accessToken: localStorage.getItem("accessToken")
           }
