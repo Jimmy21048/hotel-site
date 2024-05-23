3c24d7b9b21de5aa59b404cb7ea297e3e55ff2e0
Jimmy21048 <118855884+Jimmy21048@users.noreply.github.com>
2024-05-23 09:41:20 +0300

    Update Foods.js

--- src/components/Foods.js
+++ src/components/Foods.js
@@ -48,7 +48,9 @@ export default function Foods() {
                 accessToken: localStorage.getItem("accessToken")
             }
         }).then((response) => {
-            setBookedRooms(response.data);         
+            console.log(bookedRooms);
+            setBookedRooms(response.data); 
+            
         })
     }, [bookedRooms])
     
